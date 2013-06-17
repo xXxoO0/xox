aaa636fd959caa1f8507f0327dcf165ed052b959
美好生活 <zwxiaole@126.com>
2013-06-17 21:42:52 +0800

    最新css

--- WebContent/js/xox-common.js
+++ WebContent/js/xox-common.js
@@ -6,5 +6,19 @@
 			});
 			$(this).addClass("current");
 		});
+		//右侧返回顶部按钮
+		$("#xox-slide").find("a").on("mouseover",function(){
+			$(this).css({
+				'background': 'url(../image/bg/xox-slider.png) 0 0 no-repeat'
+			});
+		});
+		$("#xox-slide").find("a").on("mouseout",function(){
+			$(this).css({
+				'background': 'url(../image/bg/xox-slider.png) -40px 0 no-repeat'
+			});
+		});
+		$("#side-a").on("click",function(){
+			scroll(0,0);
+		});
 	});
 })(jQuery);
